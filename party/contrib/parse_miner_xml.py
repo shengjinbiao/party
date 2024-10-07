66df034de17b7e701ad26875165667d7efee1960
Benjamin Kiessling <mittagessen@l.unchti.me>
2024-10-07 14:31:29 +0200

    update pdfminer script

--- party/contrib/parse_miner_xml.py
+++ party/contrib/parse_miner_xml.py
@@ -18,7 +18,6 @@ from scipy.special import comb
 from shapely.geometry import LineString
 
 from party.codec import OctetCodec
-from party.default_specs import RECOGNITION_HYPER_PARAMS
 
 from rich.progress import track
 
@@ -56,51 +55,32 @@ def bezier_fit(bl):
 
 def _to_curve(baseline, im_size, min_points: int = 8):
     """
-    Converts poly(base)lines to Bezier curves and normalizes them.
+    Converts poly(base)lines to Bezier curves.
     """
     baseline = np.array(baseline)
     if len(baseline) < min_points:
         ls = LineString(baseline)
         baseline = np.stack([np.array(ls.interpolate(x, normalized=True).coords)[0] for x in np.linspace(0, 1, 8)])
-    # control points normalized to patch extents
+    # control points
     curve = np.concatenate(([baseline[0]], bezier_fit(baseline), [baseline[-1]]))/im_size
     curve = curve.flatten()
     return pa.scalar(curve, type=pa.list_(pa.float32()))
 
 
-def _to_bbox(coords, im_size):
+def _to_bbox(boundary, im_size):
     """
     Converts a bounding polygon to a bbox in xyxyc_xc_yhw format.
     """
-    xmin, ymin, xmax, ymax = coords
+    flat_box = [point for pol in boundary for point in pol]
+    xmin, xmax = min(flat_box[::2]), max(flat_box[::2])
+    ymin, ymax = min(flat_box[1::2]), max(flat_box[1::2])
     w = xmax - xmin
     h = ymax - ymin
     cx = (xmin + xmax) / 2
     cy = (ymin + ymax) / 2
-    return pa.scalar([xmin, ymin, xmax, ymax, cx, cy, w, h], type=pa.list_(pa.float32()))
-
-
-def optional_resize(img, max_size):
-    """
-    Resizing that return images with the longest side below `max_size`
-    unchanged.
-
-    Args:
-        img: image to resize
-        max_size: maximum length of any side of the image
-    """
-    w, h = img.size
-    img_max = max(w, h)
-    if img_max > max_size:
-        if w > h:
-            h = int(h * max_size/w)
-            w = max_size
-        else:
-            w = int(w * max_size/h)
-            h = max_size
-        return img.resize((w, h))
-    else:
-        return img
+    bbox = np.array([[xmin, ymin], [xmax, ymax], [cx, cy], [w, h]]) / im_size
+    bbox = bbox.flatten()
+    return pa.scalar(bbox, type=pa.list_(pa.float32()))
 
 
 line_struct = pa.struct([('text', pa.list_(pa.int32())), ('curve', pa.list_(pa.float32())), ('bbox', pa.list_(pa.float32()))])
@@ -108,8 +88,6 @@ page_struct = pa.struct([('im', pa.binary()), ('lines', pa.list_(line_struct))])
 schema = pa.schema([('pages', page_struct)])
 
 codec = OctetCodec()
-
-max_side_length = 4000
 
 output_file = sys.argv[1]
 
@@ -120,6 +98,8 @@ with tempfile.NamedTemporaryFile() as tmpfile:
     with pa.OSFile(tmpfile.name, 'wb') as sink:
         with pa.ipc.new_file(sink, schema) as writer:
             num_lines = 0
+            max_lines_in_page = 0
+            max_octets_in_line = 0
             for doc in track(docs, description='Reading XML files'):
                 with open(doc, 'r') as fp:
                     try:
@@ -140,6 +120,7 @@ with tempfile.NamedTemporaryFile() as tmpfile:
                                 baseline = _to_curve(np.array((x1, bl_y, x2, bl_y)).reshape(2, 2), page_dim)
                                 bbox = _to_bbox((x1, y1, x2, y2), page_dim)
                                 text = codec.encode(regex.sub(r'\s', ' ', ''.join(c.text for c in line.iterfind('text'))).strip()).numpy()
+                                max_octets_in_line = max(len(text), max_octets_in_line)
                                 page_data.append(pa.scalar({'text': pa.scalar(text),
                                                             'curve': baseline,
                                                             'bbox': bbox}, line_struct))
@@ -147,10 +128,10 @@ with tempfile.NamedTemporaryFile() as tmpfile:
                                 continue
                         # render PDF to image
                         im = pdf_page.render(scale=4).to_pil()
-                        im = optional_resize(im, RECOGNITION_HYPER_PARAMS['longest_edge']).convert('RGB')
                         fp = io.BytesIO()
                         im.save(fp, format='png')
                         num_lines += len(page_data)
+                        max_lines_in_page = max(len(page_data), max_lines_in_page)
                         # flush into arrow file
                         if len(page_data):
                             ar = pa.array([pa.scalar({'im': fp.getvalue(), 'lines': page_data}, page_struct)], page_struct)
@@ -158,7 +139,9 @@ with tempfile.NamedTemporaryFile() as tmpfile:
 
     print(f'Writing temporary file {tmpfile.name} to {output_file}')
     with pa.memory_map(tmpfile.name, 'rb') as source:
-        metadata = {'num_lines': num_lines.to_bytes(4, 'little')}
+        metadata = {'num_lines': num_lines.to_bytes(4, 'little'),
+                    'max_lines_in_page': max_lines_in_page.to_bytes(4, 'little'),
+                    'max_octets_in_line': max_octets_in_line.to_bytes(4, 'little')}
         schema = schema.with_metadata(metadata)
         ds_table = pa.ipc.open_file(source).read_all()
         new_table = ds_table.replace_schema_metadata(metadata)
