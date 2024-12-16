2a47cf7a2f629208a16242b5c365befad20a6e2b
Benjamin Kiessling <mittagessen@l.unchti.me>
2024-12-16 11:52:01 +0000

    Revert "use torchao AdamW"
    This reverts commit 357b881c493659d498099640f6cefd21054c4db9.

--- party/model.py
+++ party/model.py
@@ -241,8 +241,7 @@ def _configure_optimizer_and_lr_scheduler(hparams, params, loss_tracking_mode='m
     # XXX: Warmup is not configured here because it needs to be manually done in optimizer_step()
     logger.debug(f'Constructing {optimizer} optimizer (lr: {lr}, momentum: {momentum})')
     if optimizer in ['Adam', 'AdamW']:
-        from torchao.prototype.low_bit_optim import _AdamW
-        optim = _AdamW(params, bf16_stochastic_round=True, lr=lr, weight_decay=weight_decay)
+        optim = getattr(torch.optim, optimizer)(params, lr=lr, weight_decay=weight_decay)
     else:
         optim = getattr(torch.optim, optimizer)(params,
                                                 lr=lr,
