65eba3fc6c9e15c056b4d6ff519ae5d01abea55a
Benjamin Kiessling <mittagessen@l.unchti.me>
2024-12-20 21:48:00 +0100

    Revert "make lr change with ao adamw work"
    This reverts commit 43ab52fdb7ba6a843c3ae9c101024e7b5218be51.

--- party/model.py
+++ party/model.py
@@ -209,7 +209,7 @@ class RecognitionModel(L.LightningModule):
         if self.hparams.warmup and self.trainer.global_step < self.hparams.warmup:
             lr_scale = min(1.0, float(self.trainer.global_step + 1) / self.hparams.warmup)
             for pg in optimizer.param_groups:
-                pg['lr'].fill_(lr_scale * self.hparams.lr)
+                pg["lr"] = lr_scale * self.hparams.lr
 
     def lr_scheduler_step(self, scheduler, metric):
         if not self.hparams.warmup or self.trainer.global_step >= self.hparams.warmup:
