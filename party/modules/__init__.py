7585305a882df0e410c224f6e5a2f5a028781cb1
Benjamin Kiessling <mittagessen@l.unchti.me>
2025-02-07 20:57:56 +0100

    get rid of tiedlinear

--- party/modules/__init__.py
+++ party/modules/__init__.py
@@ -4,6 +4,5 @@ from .tanh_gate import TanhGate  # NOQA
 from .llama_components import scale_hidden_dim_for_mlp, llama3_mlp, Llama3ScaledRoPE  # NOQA
 from .fusion_layer import FusionLayer  # NOQA
 from .transformer import TransformerCrossAttentionLayer, TransformerSelfAttentionLayer, TransformerDecoder  # NOQA
-from .tied_linear import TiedLinear  # NOQA
 from .feed_forward import FeedForward  # NOQA
 from .prompt import PromptEncoder  # NOQA
